33e51e273268db358f3cecfa444f22b07f8c876b
xmnlz <lemmeq9@gmail.com>
2022-09-08 16:02:19 +0300

    implement settings

--- UserVoiceShow/src/components/SettingsPanel/SettingsPanel.tsx
+++ UserVoiceShow/src/components/SettingsPanel/SettingsPanel.tsx
@@ -5,19 +5,32 @@ import { settings } from '../../main';
 const SwitchItem = WebpackModules.getByDisplayName('SwitchItem');
 
 const SettingsPanel = (props) => {
-    const [value, setValue] = useState(settings.useProfileModal);
+    const [profileModal, setProfileModal] = useState(settings.useProfileModal);
+    const [showCategory, setShowCategory] = useState(settings.showCategory);
 
     return (
-        <SwitchItem
-            children={'Display in profile'}
-            note="When enabled, the channel will also be visible in the user profile."
-            value={value}
-            onChange={(val) => {
-                setValue(val);
-                settings.useProfileModal = val;
-                BdApi.setData('vus', 'useProfileModal', val);
-            }}
-        />
+        <>
+            <SwitchItem
+                children={'Display in profile'}
+                note="When enabled, the channel will also be visible in the user profile."
+                value={profileModal}
+                onChange={(val) => {
+                    setProfileModal(val);
+                    settings.useProfileModal = val;
+                    BdApi.setData('vus', 'useProfileModal', val);
+                }}
+            />
+            <SwitchItem
+                children={'Display category'}
+                note="When enabled, show voice channel with category."
+                value={showCategory}
+                onChange={(val) => {
+                    setShowCategory(val);
+                    settings.showCategory = val;
+                    BdApi.setData('vus', 'showCategory', val);
+                }}
+            />
+        </>
     );
 };
 
--- UserVoiceShow/src/components/VoiceChannelList/VoiceChannelList.tsx
+++ UserVoiceShow/src/components/VoiceChannelList/VoiceChannelList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { checkPermissions } from '../../utils';
 import { DiscordModules } from '@zlibrary';
 import { VoiceChannelField } from '../VoiceChannelFiled';
+import { settings } from '../../main';
 const { ChannelStore, GuildStore, ChannelActions, UserStore } = DiscordModules;
 
 const { getGuild } = GuildStore;
@@ -33,14 +34,16 @@ const VoiceChannelList = (props: VoiceChannelListProps) => {
                 if (!channel) return;
 
                 const guild = getGuild(channel.guild_id);
-                let channelName = channel.name;
+                let channelName = '';
 
                 if (guild) {
-                    channelName = guild.name + ' | ' + channelName;
+                    channelName += guild.name + ' | ';
                 }
 
-                if (channel.parent_id) {
-                    channelName += '\n| ' + (getChannel(channel.parent_id).name) + ' |';
+                channelName += channel.name;
+
+                if (channel.parent_id && settings.showCategory) {
+                    channelName += ' | ' + getChannel(channel.parent_id).name + ' | ';
                 }
 
                 return (
